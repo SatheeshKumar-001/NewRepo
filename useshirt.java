3e3dd73dfff3ca83c0445207a1aab70ddc1977a3
SatheeshKumar--001 <satheeshkumarshaa@gmail.com>
2023-02-15 12:03:32 +0530

    for loopn

--- useshirt.java
+++ useshirt.java
@@ -3,7 +3,7 @@ package for3;
 public class useshirt {
 	public static void main(String[] args) {
 		shirt s1=new shirt();
-		s1.brand="Allensolly";
+		s1.brand="PeterEngland";
 		s1.price=1499;
 		s1.color="white";
 		s1.ischecked=true;
@@ -11,13 +11,13 @@ public class useshirt {
 		shirt s2=new shirt();
 		s2.brand="Netplay";
 		s2.price=899;
-		s2.color="green";
+		s2.color="blue";
 		s2.ischecked=false;
 		
 		shirt s3=new shirt();
 		s3.brand="levis";
 		s3.price=1999;
-		s3.color="blck";
+		s3.color="black";
 		s3.ischecked=true;
 		shirt[] shirts= {s1,s2,s3};
 		for(shirt s: shirts ) {
